80a39433504dc7e670d222171e72147527e4c1a0
Dusan Tadic <47108307+dt-exploration@users.noreply.github.com>
2019-03-30 12:04:12 +0100

    style update

--- cookies.php
+++ cookies.php
@@ -1,14 +1,15 @@
 <?php
 
 //Create and modify cookie
-if(!isset($_COOKIE["id"]))  {
+if (!isset($_COOKIE["id"]))  {
     setcookie("id", "j23sj942Ajola2042p", time()+300);
 }
 
- ?>
+?>
 
 <?php
-if(isset($_COOKIE["id"])) {
+
+if (isset($_COOKIE["id"])) {
     echo $_COOKIE["id"];
 }
 
